d2030388086903acb3d1d16ada59e181d3ceb761
Mac Chollan <kholan.childs404@gmail.com>
2021-11-25 14:33:43 +0700

    simple spa

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+            <div class="container">
+                <router-link :to="{ name: 'home' }" class="navbar-brand">
+                    Home
+                </router-link>
+                <button
+                    class="navbar-toggler"
+                    data-toggle="collapse"
+                    data-target="#navbarText"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <router-link
+                                :to="{ name: 'home' }"
+                                class="nav-link"
+                            >
+                                Home
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link
+                                :to="{ name: 'about' }"
+                                class="nav-link"
+                            >
+                                About
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    watch: {
+        $route() {
+            $("#navbarCollape").collapse("hide");
+        }
+    }
+}
+</script>
